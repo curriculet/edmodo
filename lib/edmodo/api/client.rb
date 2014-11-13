ccde4c169d1df48f71989279bee69bc06fc80af9
Philip Ingram <philip.d.ingram@gmail.com>
2014-11-13 10:19:46 -0800

    add post method

--- lib/edmodo/api/client.rb
+++ lib/edmodo/api/client.rb
@@ -61,6 +61,24 @@ module Edmodo
         handle_response(response, params)
       end
 
+      # post
+      # send an HTTPS post request against Edmodos's API.
+      #
+      #
+      # @param path    [String] The path for the api endpoint (with leading slash)
+      # @param params  [Hash]   A hash of query arguments for the request (optional)
+      # @param headers [Hash]   A hash of header fields (optional).
+      #
+      #
+      # @return [String]. The raw response body (JSON is expected) Raises appropriate exception if it fails
+      # @raise Edmodo::HTTPError when any HTTP error response is received
+      def post( path, body={}, headers={} )
+        #puts "CALLING API: #{Edmodo.api_url( path )} ===#{post_options}"
+        response = self.class.post( path, { body: body.to_json, headers: headers })
+
+        handle_response(response, body)
+      end
+
       # build_get_options
       # Build the hash of options for an HTTP get request.
       #
@@ -89,6 +107,36 @@ module Edmodo
 
         get_options
       end
+
+      # # build_post_options
+      # # Build the hash of options for an HTTP post request.
+      # #
+      # # @param params  [Hash] optional. Any query parameters to add to the request.
+      # # @param params  [Hash] optional. Any data included with the request.
+      # # @param user_headers [Hash] optional. Any query parameters to add to the request.
+      # #
+      # # @return [Hash] The properly formated post_options.
+      # # @raise [Edmodo::InvalidCredentials] if the access_token is required but not supplied in the params API v1.1
+      # def build_post_options( params={}, body={}, user_headers={})
+      #   post_options = {}
+      #   query = {}
+
+      #   # Add the access token
+      #   query[:access_token] = params[:access_token] if params[:access_token]
+      #   # if Edmodo.access_token_required?
+      #   #   raise Edmodo::InvalidCredentials.new(" :access_token query parameter required when using API version #{Edmodo.api_version}") if params[:access_token].nil? || params[:access_token].empty?
+      #   #   query[:access_token] = params[:access_token] if params[:access_token]
+      #   # end
+      #   query.merge!(params)
+      #   post_options[:query] = query
+
+      #   # pass any headers
+      #   headers ={}
+      #   headers.merge!( user_headers )
+      #   post_options[:headers] = headers
+
+      #   post_options
+      # end
 
 
       private
--- spec/edmodo/api/client_spec.rb
+++ spec/edmodo/api/client_spec.rb
@@ -121,4 +121,56 @@ describe Edmodo::API::Client do
       end
     end
   end
+
+  describe "post" do
+    #
+    # Because of the conflict of Excon.stubs with VCR (they do not work at the same time)
+    # We created a errors.yml file by hand to simulate stubs that always return the desired error code
+    #
+    before(:each){
+      Edmodo.api_version = 'v1' #these are error call tests, no need for access tokens
+    }
+
+    it "should not raise an exception with response status 200" do
+      VCR.use_cassette("errors") do
+        expect{ client.post("/200") }.not_to raise_error
+      end
+    end
+    it "should raise Edmodo::BadRequest with response status is 400" do
+      VCR.use_cassette("errors") do
+        expect{ client.post("/400") }.to raise_error  Edmodo::BadRequest
+      end
+    end
+    it "should raise Edmodo::AuthenticationFailed with response status is 401" do
+      VCR.use_cassette("errors") do
+        expect{ client.post("/401") }.to raise_error  Edmodo::AuthenticationFailed
+      end
+    end
+    it "should raise Edmodo::NotFound with response status is 404" do
+      VCR.use_cassette("errors") do
+        expect{ client.post("/404") }.to raise_error  Edmodo::NotFound
+      end
+
+    end
+    it "should raise Edmodo::ServerError with response status is 500" do
+      VCR.use_cassette("errors") do
+        expect{ client.post("/500") }.to raise_error  Edmodo::ServerError
+      end
+    end
+    it "should raise Edmodo::Unavailable with response status is 502" do
+      VCR.use_cassette("errors") do
+        expect{ client.post("/502") }.to raise_error  Edmodo::Unavailable
+      end
+    end
+    it "should raise Edmodo::RateLimited. with response status is 503" do
+      VCR.use_cassette("errors") do
+        expect{ client.post("/503") }.to raise_error  Edmodo::RateLimited
+      end
+    end
+    it "should raise Edmodo::UnknownStatusCode. with response status is not listed above" do
+      VCR.use_cassette("errors") do
+        expect{ client.post("/499") }.to raise_error  Edmodo::UnknownStatusCode
+      end
+    end
+  end
 end
